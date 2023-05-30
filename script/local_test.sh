1f06a8710828b827403f678addb639e25882876a
minedec <jjdjiang@qq.com>
2023-05-30 08:56:02 +0800

    Add hash table v1, but not work

--- script/local_test.sh
+++ script/local_test.sh
@@ -1,1 +1,3 @@
-./../build/db_bench --benchmarks="fillseq,fillrandom,readrandom,readseq" --db="/home/jjd/dbtest"
+CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
+./../build/db_bench --benchmarks="fillseq,fillrandom,readrandom,readseq" --db="$CURRENT_DIR/../dbtest/"
+# --benchmarks="fillseq,fillrandom,readrandom,readseq" --db="../dbtest/"
