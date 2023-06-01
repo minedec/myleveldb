75670931efe3199bb2c662e57eb267a52eed9d38
minedec <jjdjiang@qq.com>
2023-06-01 22:26:23 +0800

    nonsense(build.sh): add gitignore

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd $CURRENT_DIR/build
 cmake -DCMAKE_BUILD_TYPE=DEBUG $CURRENT_DIR/build/..
 cmake --build $CURRENT_DIR/build
 cd $CURRENT_DIR/..
-g++ -g $CURRENT_DIR/leveldb_test.cpp -o $CURRENT_DIR/leveldbtest $CURRENT_DIR/build/libleveldb.a -I$CURRENT_DIR/include -lnuma -lpthread
+g++ -g $CURRENT_DIR/leveldb_test.cpp -o $CURRENT_DIR/leveldbtest $CURRENT_DIR/build/libleveldb.a -I$CURRENT_DIR/include -lnuma -lpthread -lsnappy
